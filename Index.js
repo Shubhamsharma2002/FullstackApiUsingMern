4223d0ece3ac49412e20e94c56a3bd12b8feff64
shubham sharma <shubhamtech004@gmail.com>
2025-06-08 22:58:11 +0530

    project setup

--- Index.js
+++ Index.js
@@ -1,13 +1,14 @@
+import 'dotenv/config';
 import express from "express";
 
 const server = express();
-
+const Port = 8000 || process.env.PORT
 // Optional: Define a simple route
 server.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
 // Start the server
-server.listen(8000, () => {
+server.listen(Port, () => {
   console.log("🚀 Server is listening on http://localhost:8000");
 });
